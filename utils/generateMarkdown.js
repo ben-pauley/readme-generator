d1f1c8622908356dfc12bcce461eb92230704fa3
Ben <pauleybj@gmail.com>
2021-02-17 14:24:57 +0000

    further markdown information rendered

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,21 +26,15 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== "Other") {
-    return (
-      "## Licence\n\n" +
-      renderLicenseBadge(license) +
-      renderLicenseLink(license)
-    );
+    return renderLicenseBadge(license) + renderLicenseLink(license);
   } else {
+    // If there is no license, return an empty string
     return "";
   }
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -71,7 +65,11 @@ function generateMarkdown(data) {
   ${data.tests}
 
   ## Questions
-  
+  You can find my GitHub profile, ${data.username}, [here](github.com/${
+    data.username
+  }).
+
+  If you have any additional questions, email me at ${data.email}.
 `;
 }
 
